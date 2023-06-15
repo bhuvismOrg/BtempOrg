c8220ff6a40d4cd93399b71a050b7e72f76ff2e2
Bhuvan Jain S M <bhuvanbhuvism@gmail.com>
2023-06-15 12:17:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -27,6 +27,7 @@
   <link href="indexStyles.css" rel="stylesheet">
   <script>
   function loader(){
+      var subtsask = 82
     var file = document.getElementById('file-upload').value
     var room = document.getElementById('roomid').value
     if(file && room){
